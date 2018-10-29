9c9979a70bc5c1b43142306d99f3b8a2f333ac4a
soldate <soldate@gmail.com>
2018-10-29 19:12:04 -0300

    json and user main method

--- src/jetty/Main.java
+++ src/jetty/Main.java
@@ -6,15 +6,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-class Main {
-
+public class Main {
+	// tes
 	private static final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
 	private static final String dbName="jdbcDemoDB";
 	private static final String connectionURL = "jdbc:derby:" + dbName + ";create=true";
 	private static Connection conn;
 	private static Main main;
 	
-	private Main() {
+	protected Main() {
 		String createString = "CREATE TABLE Log  "
 				  +  "(ID INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
 				  +  " WHO VARCHAR(256), "
@@ -33,17 +33,19 @@ class Main {
 		}		
 	}
 
-	protected void execute(String sql) throws SQLException {
+	void execute(String sql) throws SQLException {
+		getInstance();
 		Statement s = conn.createStatement();
 		s.execute(sql);		
 	}
 	
-	protected ResultSet executeQuery(String sql) throws SQLException {
+	ResultSet executeQuery(String sql) throws SQLException {
+		getInstance();
 		Statement s = conn.createStatement();
 		return s.executeQuery(sql);		
 	}
 	
-	public static Main getInstance() {
+	static Main getInstance() {
 		if (main == null) main = new Main();
 		return main;
 	}
---
+++ src/your/Main.java
@@ -0,0 +1,11 @@
+package your;
+
+import org.json.JSONObject;
+
+public class Main extends jetty.Main {
+	
+	public static JSONObject main(JSONObject json) {
+		return null;
+	}
+
+}
